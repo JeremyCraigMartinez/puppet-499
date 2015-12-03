ed1c8d5ad025b51a120d9ab256a6a0945f75f01d
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2015-12-03 23:44:44 +0000

    renamed watch to syslog

--- modules/syslog/manifests/init.pp
+++ modules/syslog/manifests/init.pp
@@ -1,8 +1,8 @@
-class watch {
-	file { 'watch.sh':
+class syslog {
+	file { 'syslog.sh':
 		ensure => 'file',
-		source => "puppet:///modules/watch/watch.sh",
-		path => '/usr/local/bin/watch.sh',
+		source => "puppet:///modules/syslog/syslog.sh",
+		path => '/usr/local/bin/syslog.sh',
 		owner => 'vagrant',
 		group => 'vagrant',
 		mode    => 0755, 
@@ -10,7 +10,7 @@ class watch {
 
 	file { 'reportToMaster.sh':
 		ensure => 'file',
-		source => "puppet:///modules/watch/reportToMaster.sh",
+		source => "puppet:///modules/syslog/reportToMaster.sh",
 		path => '/usr/local/bin/reportToMaster.sh',
 		owner => 'vagrant',
 		group => 'vagrant',
@@ -19,7 +19,7 @@ class watch {
 
 	file { 'ssmtp.conf':
 		ensure => 'present',
-		source => "puppet:///modules/watch/config/ssmtp.conf",
+		source => "puppet:///modules/syslog/config/ssmtp.conf",
 		path => '/etc/ssmtp/ssmtp.conf',
 		owner => 'root',
 		group => 'root',
@@ -49,18 +49,18 @@ class watch {
 	}	
 
 	cron { 'newSyslogInfo':   
-		command => "/usr/local/bin/watch.sh",   
+		command => "/usr/local/bin/syslog.sh",   
 		user    => root,
-		hour    => '*',   
-		minute  => '*/2',
-		require => File['watch.sh','syslog.old','syslog.diff']
+		hour    => '*',
+		minute  => '*/20', 
+		require => File['syslog.sh','syslog.old','syslog.diff']
 	}
 
 	cron { 'report':   
 		command => "/usr/local/bin/reportToMaster.sh",   
 		user    => root,
 		hour    => '*',   
-		minute  => '*/2',
+		minute  => '*/20',
 		require => [
 			File['reportToMaster.sh'],
 			Cron['newSyslogInfo']
